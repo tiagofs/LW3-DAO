f0e7c32e837030eddaf8b035faaa5a15ac28655e
Tiago Silva <tfmsilva26@gmail.com>
2022-07-22 23:21:23 +0100

    DAO hardhat and nextjs project

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x1bE1a62E1A09E8E6BE5a71618ce8f53de5770273";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
